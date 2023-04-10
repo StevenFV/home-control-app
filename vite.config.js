abf77ea5f3b111f8af07d16b163ea40d3f4f0031
StevenFV <veilletsteven@hotmail.com>
2023-04-10 14:19:24 -0400

    first commit

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+// Use this link technic for access VITE_ variables of .env
+// https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+
+export default ({mode}) =>
+{
+    // Load app-level env vars to node-level env vars.
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        server: {
+            host: '0.0.0.0',
+            hmr: {
+                // Should match the domain name for avoid "Cross-Origin Request Blocked"
+                host: process.env.VITE_HMR_HOST,
+
+                // Browser will utilise this port, use for redirect the connection to the good Docker container (to npm who listen on the port)
+                clientPort: process.env.VITE_HMR_CLIENT_PORT,
+            },
+        },
+    });
+}
