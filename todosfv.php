e9182658788743f62f002b89d1a2d5410e380dde
StevenFV <veilletsteven@hotmail.com>
2023-06-18 10:40:10 -0400

    todo: add todosfv file

---
+++ todosfv.php
@@ -0,0 +1,10 @@
+<?php
+
+// todosfv list:
+
+/*
+- phpunit, pest
+- Laravel Recurring Models https://laravel-news.com/laravel-recurring-models
+- Database backup
+- Laravel Precognition https://laravel-news.com/laravel-precognition
+*/
