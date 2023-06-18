404a5ff6844b2dd6775f963be3d5f801994cd7b1
StevenFV <veilletsteven@hotmail.com>
2023-06-18 10:40:53 -0400

    feat: add light controller

---
+++ app/Http/Controllers/Lighting/LightController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Lighting;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LightController extends Controller
+{
+    public function __construct()
+    {
+        // todosfv setup Spacie permissions package && email verification
+    }
+}
