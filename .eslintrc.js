f5239327c81669a54f1aec07abc714b908da6b6c
StevenFV <veilletsteven@hotmail.com>
2023-06-29 18:47:35 -0400

    style: formatting add eslint && update .editorconfig && .phpcs.xml

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/base',
+        'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-strongly-recommended',
+        'plugin:vue/vue3-recommended',
+    ],
+    rules: {
+        'vue/html-indent': ['error', 4, {
+            'attribute': 1,
+            'baseIndent': 1,
+            'closeBracket': 0,
+            'alignAttributesVertically': true,
+            'ignores': [],
+        }],
+    },
+    'overrides': [
+        {
+            'files': ['*.vue'],
+            'rules': {
+                'vue/multi-word-component-names': 'off',
+            },
+        },
+    ],
+    'globals': {
+        'route': 'readonly',
+        'Ziggy': 'readonly',
+    },
+}
