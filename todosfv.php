f8f006358fcb6c3e80a7d5380b1a630396b2f9c6
StevenFV <veilletsteven@hotmail.com>
2023-06-29 18:38:19 -0400

    todo: update todosfv file

--- todosfv.php
+++ todosfv.php
@@ -3,7 +3,13 @@
 // todosfv list:
 
 /*
-- phpunit, pest
+- Outils d'analyse de code (phpunit, pest, etc.):
+    - PHP_CodeSniffer: ./vendor/bin/phpcs - The file .phpcs.xml is already created
+    - PHPUnit: ./vendor/bin/phpunit
+    - Psalm: ./vendor/bin/psalm
+    - ESLint: ./node_modules/eslint/bin/eslint.js --ext .js,.vue resources/js/
+    Pas exécuté automatiquement pour le moment lors des MR:
+        PHPStan (Larastan): ./vendor/bin/phpstan analyse --xdebug
 - Laravel Recurring Models https://laravel-news.com/laravel-recurring-models
 - Database backup
 - Laravel Precognition https://laravel-news.com/laravel-precognition
