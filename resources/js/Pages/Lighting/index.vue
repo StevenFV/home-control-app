fc2ff5543d8ab6badb38250fa63b9e6a3b49e138
StevenFV <veilletsteven@hotmail.com>
2023-07-07 20:05:02 -0400

    feat: frontend two-way binding subscribe and publish state

--- resources/js/Pages/Lighting/index.vue
+++ resources/js/Pages/Lighting/index.vue
@@ -2,7 +2,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import InputSwitch from 'primevue/inputswitch';
 import {Head} from "@inertiajs/vue3";
-import {ref} from 'vue';
+import {computed, reactive, watch} from 'vue';
 import {trans} from "laravel-vue-i18n";
 
 const props = defineProps({
@@ -12,9 +12,24 @@ const props = defineProps({
     }
 })
 
-const topics = Object.keys(props.subscribeTopicMessage.lightingSubscribeMessage);
+const state = reactive({});
+// Assign initial state values
+for (const topic in props.subscribeTopicMessage.lightingSubscribeMessage) {
+    state[topic] = props.subscribeTopicMessage.lightingSubscribeMessage[topic].state === 'ON';
+}
 
-// const state = ref(subscribe.value.state === 'ON'); todosfv 2 way binding
+const lightingStates = computed(() => {
+    return Object.keys(state).map(topic => ({
+        topic,
+        state: state[topic] ? 'ON' : 'OFF'
+    }));
+});
+
+watch(lightingStates, (newStates) => {
+    // todosfv publish mqtt state and review all this solution (foreach in mqtt controller and two way binding vue.js)
+    // Call the Laravel controller function here with the new lighting states
+    // You can use AJAX, Axios, or any other method to send the data to the server
+});
 </script>
 
 <template>
@@ -25,24 +40,24 @@ const topics = Object.keys(props.subscribeTopicMessage.lightingSubscribeMessage)
             <!-- todosfv add to lang and make component -->
             <div class="grid grid-cols-3 gap-6">
                 <div
-                    v-for="topic in topics"
-                    :key="topic"
+                    v-for="lighting in lightingStates"
+                    :key="lighting.topic"
                     class="bg-yellow-100 shadow-lg rounded-lg overflow-hidden m-6 col-3"
                 >
                     <div class="text-center pt-2 font-semibold text-xl text-gray-800 leading-tight">
-                        {{ topic }}
+                        {{ lighting.topic }}
                     </div>
                     <div class="p-4">
                         {{
-                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[topic].energy
+                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[lighting.topic].energy
                         }}<br>
                         {{
-                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[topic].linkquality
+                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[lighting.topic].linkquality
                         }}<br>
                         {{
-                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[topic].power
+                            trans('Link quality: ') + props.subscribeTopicMessage.lightingSubscribeMessage[lighting.topic].power
                         }}<br>
-                        <InputSwitch v-model="state" />
+                        <InputSwitch v-model="state[lighting.topic]" />
                     </div>
                 </div>
             </div>
