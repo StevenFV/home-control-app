141417cbaf19b8f7e86fb851633078534df5f09c
StevenFV <veilletsteven@hotmail.com>
2023-07-09 12:00:16 -0400

    style: test github action eslint verification

--- todosfv.php
+++ todosfv.php
@@ -5,9 +5,8 @@
 /*
 - Outils d'analyse de code (phpunit, pest, etc.):
     - PHP_CodeSniffer: ./vendor/bin/phpcs - The file .phpcs.xml is already created
-    - PHPUnit: ./vendor/bin/phpunit
+    - PEST or PHPUnit: ./vendor/bin/phpunit
     - Psalm: ./vendor/bin/psalm
-    - ESLint: ./node_modules/eslint/bin/eslint.js --ext .js,.vue resources/js/
     Pas exécuté automatiquement pour le moment lors des MR:
         PHPStan (Larastan): ./vendor/bin/phpstan analyse --xdebug
 - Laravel Recurring Models https://laravel-news.com/laravel-recurring-models
