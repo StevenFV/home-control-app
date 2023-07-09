695220df6c2f2deef06b68d76dac6c647ab425b3
StevenFV <veilletsteven@hotmail.com>
2023-07-09 15:08:21 -0400

    todo: update todo

--- todosfv.php
+++ todosfv.php
@@ -3,12 +3,14 @@
 // todosfv list:
 
 /*
-- Outils d'analyse de code (phpunit, pest, etc.):
-    - PHP_CodeSniffer: ./vendor/bin/phpcs - The file .phpcs.xml is already created
-    - PEST or PHPUnit: ./vendor/bin/phpunit
-    - Psalm: ./vendor/bin/psalm
-    Pas exécuté automatiquement pour le moment lors des MR:
-        PHPStan (Larastan): ./vendor/bin/phpstan analyse --xdebug
+- PEST: ./vendor/bin/pest:
+    - Update dependencies - With a check scheduled
+    - Unit tests for automatic verification before push with a prompt for commit message:
+        - Delete GitHub action ESLint verification and add to this unit test
+        - PSR12
+        - PHP_CodeSniffer: ./vendor/bin/phpcs - The file .phpcs.xml is already created
+        - Psalm: ./vendor/bin/psalm
+        - PHPStan (Larastan): ./vendor/bin/phpstan analyse --xdebug
 - Laravel Recurring Models https://laravel-news.com/laravel-recurring-models
 - Database backup
 - Laravel Precognition https://laravel-news.com/laravel-precognition
