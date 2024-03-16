ef9946bde01e6f09ca32f4fdd799789fd6af9472
Steven Francoeur-Veillet <steven@cybercat.ca>
2024-03-15 21:04:52 -0400

    refactor - HCA-32: publish message mechanic

---
+++ app/Http/Requests/Devices/DeviceRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Devices;
+
+use App\DevicePolicy;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeviceRequest extends FormRequest
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $devicePolicy = new DevicePolicy();
+        $devicePolicy->check();
+    }
+
+    public function rules(): array
+    {
+        return [
+            'friendlyName' => ['required', 'string', 'max:81'],
+            'set' => ['required', 'string', 'size:4', 'in:/set'],
+            'state' => ['required', 'string'],
+            'deviceType' => ['required', 'string']
+        ];
+    }
+}
