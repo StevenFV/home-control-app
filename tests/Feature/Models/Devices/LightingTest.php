642db0757a13368aa0aa90c77af5590b026b877f
Steven Francoeur-Veillet <veilletsteven@hotmail.com>
2024-05-11 20:04:21 -0400

    test - HCA-10: add tests for Lighting

--- tests/Feature/Models/Devices/LightingTest.php
+++ tests/Feature/Models/Devices/LightingTest.php
@@ -1,7 +1,50 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\Devices\Lighting;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-    $response->assertStatus(200);
+uses(DatabaseTransactions::class);
+
+it('creates lighting model successfully', function () {
+    $lighting = Lighting::factory()->create();
+
+    $this->assertDatabaseHas('devices.lights', [
+        'ieee_address' => $lighting->ieee_address,
+        'friendly_name' => $lighting->friendly_name,
+        'brightness' => $lighting->brightness,
+        'energy' => $lighting->energy,
+        'linkquality' => $lighting->linkquality,
+        'power' => $lighting->power,
+        'state' => $lighting->state,
+        'created_at' => $lighting->created_at,
+        'updated_at' => $lighting->updated_at
+    ]);
+});
+
+it('updates lighting model successfully', function () {
+    $lighting = Lighting::factory()->create();
+
+    sleep(1); // to make sure updated_at is different (1-second delay)
+
+    $newState = $lighting->state === 'on' ? 'off' : 'on';
+    $originalUpdatedAt = $lighting->updated_at;
+
+    $lighting->update([
+        'state' => $newState
+    ]);
+
+    $this->assertTrue($lighting->updated_at > $originalUpdatedAt, "Error: updated_at isn't updated");
+
+    $this->assertDatabaseHas('devices.lights', [
+        'ieee_address' => $lighting->ieee_address,
+        'state' => $newState
+    ]);
+});
+
+it('deletes lighting model successfully', function () {
+    $lighting = Lighting::factory()->create();
+
+    $lighting->delete();
+
+    $this->assertDatabaseMissing('devices.lights', ['id' => $lighting->id]);
 });
