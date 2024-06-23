cbc254a63d086fcbbb0125d90aed734011809b1b
Steven Francoeur-Veillet <veilletsteven@hotmail.com>
2024-06-23 11:53:26 -0400

    refactor - Seed admin user

--- app/Enums/PermissionName.php
+++ app/Enums/PermissionName.php
@@ -3,10 +3,12 @@
 namespace App\Enums;
 
 use ArchTech\Enums\Names;
+use ArchTech\Enums\Values;
 
 enum PermissionName: string
 {
     use Names;
+    use Values;
 
     /*
      * When this enum is modified, the enum "resources/js/Enums/PermissionName.js"
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,61 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\PermissionName;
+use App\Enums\PermissionRole;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->resetCachedRolesPermissions();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $roles = $this->createRoles();
+        $permissions = $this->createPermissions();
+
+        foreach ($roles as $roleName) {
+            $this->assignPermissionsToRole($roleName, $permissions);
+        }
+
+        $this->seadAdminUser();
+    }
+
+    private function resetCachedRolesPermissions(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+
+    private function createRoles(): array
+    {
+        $roles = PermissionRole::values();
+
+        return array_map(function ($role) {
+            return Role::create(['name' => $role])->name;
+        }, $roles);
+    }
+
+    private function createPermissions(): array
+    {
+        $permissions = PermissionName::values();
+
+        return array_map(function ($permission) {
+            return Permission::create(['name' => $permission])->name;
+        }, $permissions);
+    }
+
+    private function assignPermissionsToRole(string $roleName, array $permissions): void
+    {
+        $role = Role::findByName($roleName);
+        $role->givePermissionTo($permissions);
+    }
+
+    private function seadAdminUser(): void
+    {
+        User::factory(1)->assignAdminRole()->create();
     }
 }
