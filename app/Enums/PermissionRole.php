85baf2bdb0917ece60366b7fd47310511b66bffe
Steven Francoeur-Veillet <veilletsteven@hotmail.com>
2024-06-23 11:53:26 -0400

    refactor - Seed admin user

--- app/Enums/PermissionRole.php
+++ app/Enums/PermissionRole.php
@@ -3,10 +3,12 @@
 namespace App\Enums;
 
 use ArchTech\Enums\Names;
+use ArchTech\Enums\Values;
 
 enum PermissionRole: string
 {
     use Names;
+    use Values;
 
     /*
      * When this enum is modified, the enum "resources/js/Enums/PermissionRole.js"
